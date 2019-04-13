0c4b1a10330d9b616b9ec11aed17eb0f1edc1830
Conner <41399281+cdavi198@users.noreply.github.com>
2019-04-13 13:14:19 -0400

    Create table.js

---
+++ cdavi198.github.io/Scripts/table.js
@@ -0,0 +1,11 @@
+var TableData = new Array();
+
+$('#gotTable tr').each(function(row, tr){
+  TableData[row]={
+    "Character" : $(tr).find('td:eq(5)').text()
+    }
+    
+    var Alys = TableData[0];
+    var Arya = TableData[1];
+  });
+  TableData.shift();
